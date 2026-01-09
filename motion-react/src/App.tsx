50c2cb43d010ceb41ef76362829813f43d8d86b3
zaidkhatri-dev <zaid.abdulgani.khatri@gmail.com>
2026-01-09 17:45:53 +0530

    feat: add routing

--- motion-react/src/App.tsx
+++ motion-react/src/App.tsx
@@ -1,8 +1,23 @@
-import FootballCard from "./projects/football-cards/FootballCard";
+import { Link } from "react-router";
 
 const App = () => {
   return (
-    <FootballCard />
+    <div className="min-h-screen w-screen flex items-center justify-center">
+      <nav>
+        <ul className="flex items-center justify-center gap-20 text-2xl bg-gray-100 relative text-neutral-900 rounded-full">
+          <li className="px-5 py-3">
+            <Link to="/move-me">
+              <span>Move Me</span>
+            </Link>
+          </li>
+          <li className="px-5 py-3">
+            <Link to="/football-cards">
+              <span>Football Cards</span>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 };
 
--- motion-react/src/main.tsx
+++ motion-react/src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { BrowserRouter, Routes, Route } from "react-router";
+import FootballCard from "./projects/football-cards/FootballCard.tsx";
+import MoveMe from "./projects/move-me/MoveMe.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}></Route>
+        <Route path="football-cards" element={<FootballCard />}></Route>
+        <Route path="move-me" element={<MoveMe />}></Route>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
